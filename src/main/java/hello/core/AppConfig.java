6c28c9d9ae533f5d968b26d009c242db4b07b776
Helenhyerim <hr4658@naver.com>
2021-11-23 13:00:52 +0900

    Test finding Spring Beans

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 구현 객체를 생성하고 연결함으로써 app 전반의 구성을 책임지는 Config
+@Configuration // 설정,구성 정보
 public class AppConfig {
 
     // 생성자 주입, DI
     // 리팩토링(중복 제거, 역할과 구현의 클래스가 한눈에 들어오게)
+    // Spring Container 에 등록
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
